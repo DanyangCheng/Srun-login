b0ac797e8f73d48120b51e2c5c033a1a54f7d77a
D.Y.Cheng <dycheng.tech@outlook.com>
2026-05-19 10:55:40 +0800

    Amend data type.

--- args.py
+++ args.py
@@ -5,6 +5,6 @@ def parse_args():
     parser.add_argument('--username',type=str,default="24451352007",help="your user name.")
     parser.add_argument('--passwd',type=str,default="c*1764398966",help="your password.")
     parser.add_argument('--login_host',type=str,default="192.168.16.66",help="login host.")
-    parser.add_argument('--ip',type=int,default="",help="your ip.")# 局域网ip
+    parser.add_argument('--ip',type=str,default="",help="your ip.")# 局域网ip
     args = parser.parse_args()
     return args
